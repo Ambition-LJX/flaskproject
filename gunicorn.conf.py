2a17252dadb6f0233a01847ffcc22269d32bbeb1
Ambition-LJX <19833722688@163.com>
2025-04-30 12:59:52 +0800

    提交

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 import multiprocessing
 
-bind = '127.0.0.1:8200'
+bind = '0.0.0.0:8200'
 workers = multiprocessing.cpu_count() * 2 + 1 # 进程数量
 threads = 10
 accesslog = "/var/log/pythonbbs/access.log"
